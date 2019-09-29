c08ec3b848c34d0098805ee42a0488aabea15558
EverOddish <everoddish@gmail.com>
2019-09-29 09:38:49 -0400

    Renamed database router file

--- DrFujiBot_Django/DrFujiBot_Django/settings.py
+++ DrFujiBot_Django/DrFujiBot_Django/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
     }
 }
 
-DATABASE_ROUTERS = ['DrFujiBot_Django.router.WestwoodDatabaseRouter']
+DATABASE_ROUTERS = ['DrFujiBot_Django.westwood_router.WestwoodDatabaseRouter']
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
