2bec5c2a7848cb2caa629e71295454f09d1c2ea6
EverOddish <everoddish@gmail.com>
2019-10-15 11:08:12 -0400

    Weekly database backup task

---
+++ DrFujiBot_Django/backup_task/backup_task.py
@@ -0,0 +1,18 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
+from django.conf import settings
+import os
+import shutil
+
+def backup_database():
+    original_path = settings.DATABASES['default']['NAME']
+    now = datetime.now()
+    filename = 'db_' + now.strftime('%Y-%m-%d_%H-%M-%S') + '.sqlite3'
+    backup_path = os.path.join(os.getenv('LOCALAPPDATA'), 'DrFujiBot_Backup', filename)
+    print('Backing up database from ' + original_path + ' to ' + backup_path)
+    shutil.copyfile(original_path, backup_path)
+
+def start():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(backup_task.backup_database, 'interval', weeks=1)
+    scheduler.start()
--- DrFujiBot_Django/dashboard/apps.py
+++ DrFujiBot_Django/dashboard/apps.py
@@ -1,5 +1,8 @@
+from backup_task import backup_task
 from django.apps import AppConfig
-
 
 class DashboardConfig(AppConfig):
     name = 'dashboard'
+
+    def ready(self):
+        backup_task.start()
