2fe0e80eb74fb73ba71b8c53ca08393c3b01e0aa
EverOddish <everoddish@gmail.com>
2019-10-20 21:02:43 -0400

    Check if a base game match can be found when a ROM hack is not found

--- DrFujiBot_Django/dashboard/lookup_helpers.py
+++ DrFujiBot_Django/dashboard/lookup_helpers.py
@@ -1,7 +1,7 @@
 from django.core.cache import cache
 from spellchecker import SpellChecker
 
-from westwood.models import Ability, Game, GamesListElement, Move, Pokemon, PokemonForm
+from westwood.models import Ability, Game, GamesListElement, Move, Pokemon, PokemonForm, RomHack
 
 def is_game_name_in_game_list(current_game_name, game_list_id):
     games_list_element_objects = GamesListElement.objects.filter(list_id=game_list_id)
@@ -9,6 +9,16 @@ def is_game_name_in_game_list(current_game_name, game_list_id):
         game = games_list_element.element
         if game.name == current_game_name:
             return True
+
+    # Check if any ROM hacks match
+    rom_hack_matches = RomHack.objects.filter(title__iexact=current_game_name)
+    if len(rom_hack_matches) > 0:
+        base_game = rom_hack_matches[0].base_game
+        for games_list_element in games_list_element_objects:
+            game = games_list_element.element
+            if game.name == base_game:
+                return True
+
     return False
 
 def correct_pokemon_name(name_to_correct):
