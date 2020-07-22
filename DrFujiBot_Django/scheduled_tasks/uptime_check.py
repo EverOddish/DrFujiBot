d0c5f25f875857e3bd7c92d154a2912a34106bc6
EverOddish <everoddish@gmail.com>
2020-07-22 08:02:39 -0400

    Force an update of the stream start time if current uptime is longer than 16 hours

--- DrFujiBot_Django/scheduled_tasks/uptime_check.py
+++ DrFujiBot_Django/scheduled_tasks/uptime_check.py
@@ -22,8 +22,17 @@ def check_uptime():
 
     start_time = get_stream_start_time()
     if start_time:
-        # Stream is live
-        if last_start_time:
+        # Stream is live 
+
+        # If our current uptime has become stale ("live" longer than 16 hours), force an update.
+        # This could happen if the computer has gone to sleep and has woken up later.
+        force_update = False
+        if None != real_start_time:
+            current_uptime = now - real_start_time
+            if current_uptime > datetime.timedelta(hours=16):
+                force_update = True
+
+        if last_start_time and False == force_update:
             # Stream is still live since last check (may have gone offline and come back in between checks)
             pass
         else:
