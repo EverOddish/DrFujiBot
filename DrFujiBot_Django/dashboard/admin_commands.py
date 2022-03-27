e50acaf4bc73ffbe72ceab97b78386db1355f1bb
EverOddish <everoddish@gmail.com>
2022-03-27 09:02:36 -0400

    Bumped DrFujiBot version to 2.0.23

--- DrFujiBot_Django/dashboard/admin_commands.py
+++ DrFujiBot_Django/dashboard/admin_commands.py
@@ -562,7 +562,7 @@ def handle_shoutout(args):
 
 def handle_debug(args):
     # Not sure what else would be useful to put here.
-    output = 'DrFujiBot 2.0.22'
+    output = 'DrFujiBot 2.0.23'
     return output
 
 def handle_afflict(args):
