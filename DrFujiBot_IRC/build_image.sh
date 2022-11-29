407af70cb6c651db751dec6010dc2e8e33ccfd60
EverOddish <everoddish@gmail.com>
2022-11-28 21:02:32 -0500

    Build and push

--- DrFujiBot_IRC/build_image.sh
+++ DrFujiBot_IRC/build_image.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker build -t drfujibot_irc .
+docker build -t drfujibot .
+docker tag drfujibot:latest 256140028858.dkr.ecr.us-east-2.amazonaws.com/drfujibot:latest
+docker push 256140028858.dkr.ecr.us-east-2.amazonaws.com/drfujibot:latest
